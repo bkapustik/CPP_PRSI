2c46d94bc38e1cc7086741c5c3f714c7afd67a07
kapustb <boriskokap@gmail.com>
2023-06-06 12:00:46 +0200

    Tests

--- main.cpp
+++ main.cpp
@@ -7,54 +7,54 @@ using namespace sf;
 
 int main()
 {
-	vector<unique_ptr<TestBase>> tests = vector<unique_ptr<TestBase>>();
-	tests.push_back(make_unique<DeckTests>());
-	tests.push_back(make_unique<PlayerTests>());
-	tests.push_back(make_unique<GameManagerTests>());
+	// vector<unique_ptr<TestBase>> tests = vector<unique_ptr<TestBase>>();
+	// tests.push_back(make_unique<DeckTests>());
+	// tests.push_back(make_unique<PlayerTests>());
+	// tests.push_back(make_unique<GameManagerTests>());
 
-	for (int i = 0; i < tests.size(); ++i)
-	{
-		tests[i]->execute();
-	}
-	// float secondsToWaitBetweenEachRound = 0.5;
-	// float screenWidth = 1920;
-	// float screenHeight = 1080;
-	
-	// RenderWindow window(VideoMode(screenWidth, screenHeight), "Prsi");
-	// auto clock = Clock();
-
-	// Event event;
-
-	// Menu menu = Menu(screenWidth, screenHeight, secondsToWaitBetweenEachRound);
-
-	// while (window.isOpen()) {
-
-	// 	while (window.pollEvent(event)) {
-
-	// 		if (event.type == Event::Closed)
-	// 		{
-	// 			window.close();
-	// 		}
-	// 		if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left)
-	// 		{
-	// 			if (!menu.gameManager.userInputReceived)
-	// 			{
-	// 				menu.playOneTurn();
-
-	// 				clock.restart();
-	// 			}
-	// 			menu.tryReactToMenuEvent();
-	// 		}
-	// 	}
-
-	// 	window.clear(Color::White);
-
-	// 	window.draw(menu.background);
-
-	// 	menu.render(clock, window);
-
-	// 	window.display();
+	// for (int i = 0; i < tests.size(); ++i)
+	// {
+	// 	tests[i]->execute();
 	// }
+	float secondsToWaitBetweenEachRound = 0.5;
+	float screenWidth = 1920;
+	float screenHeight = 1080;
+	
+	RenderWindow window(VideoMode(screenWidth, screenHeight), "Prsi");
+	auto clock = Clock();
+
+	Event event;
+
+	Menu menu = Menu(screenWidth, screenHeight, secondsToWaitBetweenEachRound);
+
+	while (window.isOpen()) {
+
+		while (window.pollEvent(event)) {
+
+			if (event.type == Event::Closed)
+			{
+				window.close();
+			}
+			if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left)
+			{
+				if (!menu.gameManager.userInputReceived)
+				{
+					menu.playOneTurn();
+
+					clock.restart();
+				}
+				menu.tryReactToMenuEvent();
+			}
+		}
+
+		window.clear(Color::White);
+
+		window.draw(menu.background);
+
+		menu.render(clock, window);
+
+		window.display();
+	}
 
 	return 0;
 }
